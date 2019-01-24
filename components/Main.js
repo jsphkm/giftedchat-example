91f23e0b5c8ad8aa691b37d9a65fb42e0afbbd18
jsphkm <jsphkm12@gmail.com>
2019-01-23 18:07:16 -0800

    Add on change event to the input

--- components/Main.js
+++ components/Main.js
@@ -1,24 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import {
   StyleSheet,
   TextInput,
   View,
 } from 'react-native';
-
-class Main extends React.Component {
-  state = { name: ''};
-  render() {
-    return (
-      <View>
-        <TextInput
-          style={styles.nameInput}
-          placeHolder="John Doe"
-          value={this.state.name}
-        />
-      </View>
-    );
-  }
-}
 
 const offset = 24;
 const styles = StyleSheet.create({
@@ -30,5 +16,31 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
 });
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+    };
+  }
+
+  onChangeText(name) {
+    this.setState({ name });
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          onChangeText={name => this.onChangeText(name)}
+          style={styles.nameInput}
+          placeHolder="John Doe"
+          value={this.state.name}
+        />
+      </View>
+    );
+  }
+}
 
 export default Main;
